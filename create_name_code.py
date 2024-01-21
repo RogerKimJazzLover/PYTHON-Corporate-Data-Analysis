1e72fc6786df569a6bca2ba66063f12d06fecd10
RogerKimJazzLover <minseungkim1017@gmail.com>
2024-01-21 19:45:23 +0900

    DONE feature: initialize data

--- create_name_code.py
+++ create_name_code.py
@@ -8,5 +8,5 @@ KOP_list = fdr.StockListing('KOSPI')
 KOP_list = KOP_list.loc[0:100, ['Code', 'Name']]
 name_code = dict(zip(KOP_list['Name'], KOP_list['Code']))
 
-with open('../Invest/Data/name_code.pkl', 'wb') as f:
+with open('./Data/name_code.pkl', 'wb') as f:
     pickle.dump(name_code, f)
